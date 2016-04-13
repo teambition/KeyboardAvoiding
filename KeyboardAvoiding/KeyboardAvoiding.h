5a2330c4713b6a68f7483ebc5e6b8a9969b97f15
Zhu Shengqi <shengqi@teambition.com>
2016-04-13 11:23:35 +0800

    Initial Commit

---
+++ KeyboardAvoiding/KeyboardAvoiding.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardAvoiding.h
+//  KeyboardAvoiding
+//
+//  Created by Zhu Shengqi on 4/13/16.
+//  Copyright © 2016 dia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardAvoiding.
+FOUNDATION_EXPORT double KeyboardAvoidingVersionNumber;
+
+//! Project version string for KeyboardAvoiding.
+FOUNDATION_EXPORT const unsigned char KeyboardAvoidingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardAvoiding/PublicHeader.h>
+
+
